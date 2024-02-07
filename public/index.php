a45e112aed835ffc1cbae092a7f52f44e4948633
Quentin Cerny <quentin.cerny@gmail.com>
2024-02-07 15:58:23 +0100

    Adds compose for mailhog, adds course docs

--- public/index.php
+++ public/index.php
@@ -2,4 +2,27 @@
 
 require './../vendor/autoload.php';
 
+use Monolog\Logger;
+use Monolog\Handler\GelfHandler;
+use Gelf\Transport\TcpTransport;
+use Gelf\Publisher;
+
+// @see https://github.com/PHPMailer/PHPMailer?tab=readme-ov-file#a-simple-example
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+
+$logger = new Logger('my_logger');
+$logger->pushHandler(
+    new GelfHandler(
+        new Publisher(
+            new TcpTransport('logstash', 5000)
+        )
+    )
+);
+
 echo "Hello World !";
+
+$logger->info('Hello world !', [
+    'lorem' => 'ipsum'
+]);
